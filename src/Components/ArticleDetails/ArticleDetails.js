bca9790d47c2f54f25bc513ffb992f013a3e7118
Carissa Gross <carissamgross@gmail.com>
2023-01-04 10:52:10 -0700

    Update Article Details component and add external link to article

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import './ArticleDetails.css'
+import { Link } from "react-router-dom";
 
-const ArticleDetails = ({ article }) => 
-{
-    console.log({article})
-
+const ArticleDetails = ({ article }) => {
+    console.log({ article })
     return (
         <section className='details-container'>
             <div className='article-details'>
                 <p className='article-title'>{article.title}</p>
-                <p className='article-byline'>{article.byline}</p>
                 <p className='description'>{article.abstract}</p>
-                <p className='article-url'>{article.url}</p>
+                <p className='article-byline'>{article.byline}</p>
+                <a href={article.short_url} target="_blank">
+                    <u>View Full Article</u>
+                </a>
             </div>
             <div className='image-wrapper'>
                 <img src={article.multimedia[0].url} alt={article.title} />
             </div>
-            <button>Home</button>
+            <Link to='/'><button className='home-button'>HOME</button>
+            </Link>
         </section>
     )
 }
